0dac9c541cc4d7e8a3cd5da4028cc85b02793af8
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-16 19:01:44 +0530

    Created variables for Room class.

--- src/Projects/HotelBookingSite/Rooms.java
+++ src/Projects/HotelBookingSite/Rooms.java
@@ -1,4 +1,8 @@
 package Projects.HotelBookingSite;
 
 public class Rooms {
+    private String no_of_beds;
+    private String room_size;
+    private String[] facilities;
+    private String suggested_for;
 }
