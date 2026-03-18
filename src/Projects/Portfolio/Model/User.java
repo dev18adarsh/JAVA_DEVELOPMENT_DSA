2ab347bc5f367e4bccbbf753f0813211e8671d59
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-18 22:14:43 +0530

    Created User class(models).

--- src/Projects/Portfolio/Model/User.java
+++ src/Projects/Portfolio/Model/User.java
@@ -1,4 +1,11 @@
 package Projects.Portfolio.Model;
 
 public class User {
+    private String username;
+    private String password;
+    private String name;
+    private String age;
+    private String address;
+    private String email;
+    private String[] skills;
 }
