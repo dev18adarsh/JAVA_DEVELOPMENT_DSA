0b55c61ca9085c2d83a492879bd9d79014e2e88b
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-18 22:23:50 +0530

    Added methods for User class.

--- src/Projects/Portfolio/Model/User.java
+++ src/Projects/Portfolio/Model/User.java
@@ -8,4 +8,56 @@ public class User {
     private String address;
     private String email;
     private String[] skills;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String[] getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String[] skills) {
+        this.skills = skills;
+    }
 }
