deed9b24afa6e98c80409e88bb6207fd1c68d848
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-19 11:06:19 +0530

    Added all variables and methods for Person class.

---
+++ src/Projects/Portfolio/Model/Person.java
@@ -0,0 +1,36 @@
+package Projects.Portfolio.Model;
+
+public class Person {
+    private String name;
+    private String id;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
