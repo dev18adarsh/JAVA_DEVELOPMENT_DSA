23489b890de7ba9b3258312947f54b7c6b99f358
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-20 14:21:29 +0530

    Created variables and methods for Person class.

---
+++ src/Projects/Portfolio/Person.java
@@ -0,0 +1,44 @@
+package Projects.Portfolio;
+
+public class Person extends User{
+    public Person(String username, String password) {
+        super(username, password);
+    }
+
+    private String name;
+    private int age;
+    private String address;
+    private String[] skills;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String[] getSkills(){
+        return skills;
+    }
+
+    public void setSkills(String[] skills){
+        this.skills = skills;
+    }
+}
