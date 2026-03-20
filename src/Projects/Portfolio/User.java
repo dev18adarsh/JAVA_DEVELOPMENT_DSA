e572c9f141ab3c87659f4ead3fd128e90037231e
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-20 15:22:23 +0530

    Few changes to be made in GenerateOTP() method.

--- src/Projects/Portfolio/User.java
+++ src/Projects/Portfolio/User.java
@@ -1,6 +1,7 @@
 package Projects.Portfolio;
 
-import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
 
 public class User {
     private String username;
@@ -25,9 +26,23 @@ public class User {
         return username;
     }
 
-    public void changePassword(String old_pass,String new_pass){
-        if(Objects.equals(old_pass, password)){
+    public int GenerateOTP(){
+        Random rand = new Random();
+        int min = 1000;
+        int max = 9999;
+        return rand.nextInt(max,min);
+    }
+
+    public void changePassword(int otp,String new_pass){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter otp : ");
+        int OTP = in.nextInt();
+        if(otp == OTP){
             this.password = new_pass;
+            System.out.println("Password changed successfully.");
+        }
+        else{
+            System.out.println("Incorrect OTP.");
         }
     }
 }
