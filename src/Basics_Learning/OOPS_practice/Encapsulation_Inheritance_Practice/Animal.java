7bd9941f019aaa2e6337ec8bf959311500e7e654
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-23 22:32:33 +0530

    Practiced Q5.{Overriding}

---
+++ src/Basics_Learning/OOPS_practice/Encapsulation_Inheritance_Practice/Animal.java
@@ -0,0 +1,7 @@
+package Basics_Learning.OOPS_practice.Encapsulation_Inheritance_Practice;
+
+public class Animal {
+    public void Sound(){
+        System.out.println("Make Sound.");
+    }
+}
---
+++ src/Basics_Learning/OOPS_practice/Encapsulation_Inheritance_Practice/Dog.java
@@ -0,0 +1,8 @@
+package Basics_Learning.OOPS_practice.Encapsulation_Inheritance_Practice;
+
+public class Dog extends Animal{
+    @Override
+    public void Sound(){
+        System.out.println("Bark.");
+    }
+}
