47d82e9d2a0ec277388efc1df3f8f10b89856df1
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-23 21:54:49 +0530

    Practiced Q3.

---
+++ src/Basics_Learning/OOPS_practice/Encapsulation_Inheritance_Practice/Main.java
@@ -0,0 +1,9 @@
+package Basics_Learning.OOPS_practice.Encapsulation_Inheritance_Practice;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.PrintAge();
+        p1.PrintName();
+    }
+}
---
+++ src/Basics_Learning/OOPS_practice/Encapsulation_Inheritance_Practice/Person.java
@@ -0,0 +1,14 @@
+package Basics_Learning.OOPS_practice.Encapsulation_Inheritance_Practice;
+
+public class Person {
+    String name = "Adarsh";
+    int age = 15;
+
+    public void PrintName(){
+        System.out.println(name);
+    }
+
+    public void PrintAge(){
+        System.out.println(age);
+    }
+}
