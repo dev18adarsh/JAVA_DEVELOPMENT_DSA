5fe8732854e6008adb057b4eff58e86bcc0ab98b
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-24 11:28:56 +0530

    Created variables and constructor for User class.

---
+++ src/Projects/UserManagementSystem/User.java
@@ -0,0 +1,12 @@
+package Projects.UserManagementSystem;
+
+public class User extends Person{
+    private String username;
+    private String password;
+
+    public User(String username,String password,String name,int age){
+        super(name,age);
+        this.username = username;
+        this.password = password;
+    }
+}
