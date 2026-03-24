91c53ec6cb0eb42e22b365c53599162336f6a3ac
dev18adarsh <adarsh.pollai@gmail.com>
2026-03-24 11:42:09 +0530

    Created UserManagementSystem by applying principles of Inheritance and Encapsulation.

---
+++ src/Projects/UserManagementSystem/Main.java
@@ -0,0 +1,9 @@
+package Projects.UserManagementSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        User u1 = new User("adarsh_7678","adarsh44","Adarsh Kumar Pollai",15);
+        String user = u1.getUsername();
+        System.out.println(user);
+    }
+}
